96774a75cbdac4edaf8fe0d784d74253535550b1
RuslanKhabibullin <khabibullin.ruslan.95@gmail.com>
2021-01-17 20:32:24 +0300

    add default_url_options to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "stripe_test_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV.fetch("HOST", "http://127.0.0.1:3000") }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
